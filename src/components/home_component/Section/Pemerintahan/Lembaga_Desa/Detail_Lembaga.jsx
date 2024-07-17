862f6e742338dfdda142f0d6764b72bc86aae8a5
Derida21 <168514230+Derida21@users.noreply.github.com>
2024-07-17 16:50:51 +0700

    desa

--- src/components/home_component/Section/Pemerintahan/Lembaga_Desa/Detail_Lembaga.jsx
+++ src/components/home_component/Section/Pemerintahan/Lembaga_Desa/Detail_Lembaga.jsx
@@ -1,35 +1,34 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const DetailLembaga = () => {
-  const lembagaList = [
-    {
-      id: 1,
-      title: "Lembaga Pencari Janda",
-      image: "/src/assets/img/logo-berau3.png",
-      description:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam maxime eius aut dolorem ipsum sint iste. Ut fugit, ipsum, velit, eius obcaecati necessitatibus impedit in enim iure architecto at illum. Error atque saepe, dolore provident possimus quasi molestias vero soluta similique incidunt aliquam harum hic, sapiente, explicabo cumque assumenda ratione deserunt ut! Quas dolores rem ad distinctio. Rerum, iusto harum. Aperiam omnis saepe porro neque commodi quos voluptatum earum? Accusantium repudiandae eveniet harum blanditiis fugit molestias impedit asperiores iste nemo, officiis cumque sit inventore! Iusto dignissimos ratione saepe tenetur voluptate.",
-      slug: "lembaga-1",
-    },
-  ];
+  const { uuid } = useParams();
+  const [detail, setDetail] = useState(null);
 
-  const { slug } = useParams();
-  const [data, setData] = useState(null);
+  const getDetail = async () => {
+    try {
+      const response = await axios.get(
+        `http://nurul-huda.org/api/lembaga/${uuid}`
+      );
+      setDetail(response.data.data);
+    } catch (error) {
+      console.error('Error fetching detail:', error);
+    }
+  };
 
   useEffect(() => {
-    const selectedLembaga = lembagaList.find((item) => item.slug === slug);
-    setData(selectedLembaga);
-  }, [slug]);
+    getDetail();
+  }, [uuid]);
 
   return (
-    <section className="px-5 md:px-[60px] lg:px-[80px] xl:px-[160px]">
-      <div className="flex flex-col pt-16 pb-5 md:pt-[120px] md:pb-[40px] lg:pt-[120px] lg:pb-20">
-        {data && (
-          <h1 className="font-[Poppins] py-3 px-4 bg-teal-700 text-[16px] md:text-[20px] font-semibold text-white rounded-t-lg border-b">
-            {data.title}
+    <section className='px-5 md:px-[60px] lg:px-[80px] xl:px-[160px]'>
+      <div className='flex flex-col pt-16 pb-5 md:pt-[120px] md:pb-[40px] lg:pt-[120px] lg:pb-20'>
+        {detail && (
+          <h1 className='font-[Poppins] py-3 px-4 bg-teal-700 text-[16px] md:text-[20px] font-semibold text-white rounded-t-lg border-b'>
+            {detail.nama}
           </h1>
         )}
-        <div className="flex flex-col gap-2 lg:gap-5 px-2 py-4 md:p-5 border bg-white shadow-lg rounded-b-lg "></div>
+        <div className='flex flex-col gap-2 lg:gap-5 px-2 py-4 md:p-5 border bg-white shadow-lg rounded-b-lg '></div>
       </div>
     </section>
   );
--- src/pages/Pemerintahan/Lembaga_Desa.jsx
+++ src/pages/Pemerintahan/Lembaga_Desa.jsx
@@ -9,20 +9,21 @@ const LembagaDesa = () => {
 
   const getData = async () => {
     try {
-      const response = await axios.get('');
+      const response = await axios.get('http://nurul-huda.org/api/lembaga');
+      setData(response.data.data);
     } catch {}
   };
 
   useEffect(() => {
     getData();
   }, []);
+
   const lembagaList = [
     {
       id: 1,
       title: 'Lembaga sdasd asdsadas asdasdasd adas',
       image: logo,
-      description:
-        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam maxime eius aut dolorem ipsum sint iste. Ut fugit, ipsum, velit, eius obcaecati necessitatibus impedit in enim iure architecto at illum. Error atque saepe, dolore provident possimus quasi molestias vero soluta similique incidunt aliquam harum hic, sapiente, explicabo cumque assumenda ratione deserunt ut! Quas dolores rem ad distinctio. Rerum, iusto harum. Aperiam omnis saepe porro neque commodi quos voluptatum earum? Accusantium repudiandae eveniet harum blanditiis fugit molestias impedit asperiores iste nemo, officiis cumque sit inventore! Iusto dignissimos ratione saepe tenetur voluptate.',
+      description: '',
       slug: 'lembaga-1',
     },
     {
@@ -42,12 +43,15 @@ const LembagaDesa = () => {
           Lembaga Kampung
         </h1>
         <div className='flex flex-col gap-2 lg:gap-5 px-2 py-4 md:p-5 border bg-white shadow-lg rounded-b-lg '>
-          {lembagaList.map((lembaga) => {
+          {data.map((item, index) => {
             return (
-              <div className='md:flex md:gap-5 space-y-2 rounded border border-teal-700 p-3 lg:p-5'>
+              <div
+                key={index}
+                className='md:flex md:gap-5 space-y-2 rounded border border-teal-700 p-3 lg:p-5'
+              >
                 <div className='flex w-full border  rounded p-4 items-center justify-center'>
                   <img
-                    src={lembaga.image}
+                    src={item.logo}
                     alt=''
                     className='max-h-32 md:max-h-36 items-center'
                   />
@@ -55,17 +59,17 @@ const LembagaDesa = () => {
                 <div className=' md py-2 space-y-1 lg:space-y-2 font-[Poppins]'>
                   <div className='flex md:max-lg:gap-2 lg:items-center justify-between'>
                     <h1 className='text-teal-700 font-medium text-[12px] lg:text-[20px]'>
-                      {lembaga.title}
+                      {item.nama} ({item.singkatan})
                     </h1>
                     <Link
-                      to={`/pemerintahan/lembaga-kampung/detail-lembaga/${lembaga.slug}`}
+                      to={`/pemerintahan/lembaga-kampung/detail-lembaga/${item.uuid}`}
                       className='cursor-pointer text-[8px] lg:text-[10px] text-white py-1 px-2 bg-teal-700 rounded h-fit'
                     >
                       Detail
                     </Link>
                   </div>
                   <p className='text-gray-500 text-[10px] lg:text-[14px] text-justify line-clamp-2 lg:line-clamp-3'>
-                    {lembaga.description}
+                    {item.deskripsi}
                   </p>
                 </div>
               </div>
